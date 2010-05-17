b4c4b60921c54efd363802d3bdae2a53b4afc7ce
Sergio Espeja <sergio.espeja@gmail.com>
2010-05-17 09:53:01 +0200

    View proposals feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,8 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+    when /^"(.*)" proposal page$/i
+      proposal_path(Proposal.find_by_name($1))
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
