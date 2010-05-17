991ac7b984ec539faf637f0412b2062b21eaad0d
Sergio Espeja <sergio.espeja@gmail.com>
2010-05-17 08:30:33 +0200

    Add validations and relations to proposals and signatures

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,3 +1,6 @@
 class Proposal < ActiveRecord::Base
+  has_many :signatures
   acts_as_list
+  
+  validates_presence_of :name, :problem, :howto_solve
 end
--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,2 +1,5 @@
 class Signature < ActiveRecord::Base
+  belogs_to :proposal
+  
+  validates_presence_of :proposal_id
 end
