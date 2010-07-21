1cf21891d0816dfc64dbbc575f969efc85a25828
Santi <santi@webfordesigners.com>
2010-07-21 09:25:00 +0200

    added como funciona page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
 
   def about_us; end
+  
+  def como_funciona; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "proposals"
   map.about_us 'sobre_nosotros', :controller => "main", :action => "about_us"
+  map.como_funciona 'como_funciona', :controller => "main", :action => "como_funciona"
 
   # See how all your routes lay out with "rake routes"
 
