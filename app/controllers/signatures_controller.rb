629743aeb6660abfa2e709f0c3bb5b029b148f25
Sergio Espeja <sergio.espeja@gmail.com>
2011-01-14 21:19:40 +0100

    Show signature error with js alert

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -6,7 +6,8 @@ class SignaturesController < ApplicationController
       tractis_signature_request = TractisApi.signature_request @signature
       redirect_to tractis_signature_request[:location]
     else
-      raise "Error al crear la firma"
+      flash[:error] = "Error al crear la firma, email no válido."
+      redirect_to proposal_url(@signature.proposal)
     end
   end
   
