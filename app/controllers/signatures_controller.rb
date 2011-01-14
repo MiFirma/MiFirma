e706fa95e02bdf79085a3a0a5953614f273c3066
Sergio Espeja <sergio.espeja@gmail.com>
2011-01-14 21:13:57 +0100

    Fix fb like and proposal urls + only redirect to tractis valid signatures

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -2,8 +2,12 @@ class SignaturesController < ApplicationController
   
   def create
     @signature = Signature.create params[:signature]
-    tractis_signature_request = TractisApi.signature_request @signature
-    redirect_to tractis_signature_request[:location]
+    if @signature.valid?
+      tractis_signature_request = TractisApi.signature_request @signature
+      redirect_to tractis_signature_request[:location]
+    else
+      raise "Error al crear la firma"
+    end
   end
   
 end
