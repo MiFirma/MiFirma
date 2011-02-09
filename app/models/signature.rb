2b604237e8e949b4df0a9b5d7a3bbe517c4511c2
Sergio Espeja <sergio.espeja@gmail.com>
2011-02-09 22:08:33 +0100

    Finish first part of integration

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -8,20 +8,35 @@ class Signature < ActiveRecord::Base
   delegate :tractis_template_code, :to => :proposal
   
   before_validation :generate_token, :set_default_state
+  
+  named_scope :signed, :conditions => ["state > 0"]
 
   STATES = [:pending, :verified, :canceled]
 
   def return_url
     "http://www.mifirma.com/signatures/#{token}"
   end
+  
+  def contract_code
+    tractis_contract_location.split("/")[4]
+  end
+  
+  def check_tractis_signature!
+    signed = TractisApi.contract_signed? contract_code    
+    self.update_attribute :state, 1 if signed
+  end
+  
+  def signed?
+    state > 0
+  end
 
   protected
   def generate_token
-    self.token = ActiveSupport::SecureRandom.hex(15)
+    self.token = ActiveSupport::SecureRandom.hex(15) if self.token.nil?
   end
     
   def set_default_state
-    self.state = 0
+    self.state = 0 if self.state.nil?
   end
   
 end
