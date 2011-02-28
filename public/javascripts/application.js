82357e7acea0a6b2077077ffe4d4a027e0577d82
Jaime Ascanio <jaimeascanio@gmail.com>
2011-02-28 12:34:55 +0100

    fixes de mierda

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     closeImage   : '/images/closelabel.png'
   }); 
   
-  var top = 10;
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 270){
-      $('.signature_box').stop().animate({ top: $(window).scrollTop() + top}, 500);
-    }           
-  });
+  // var top = 10;
+  // $(window).scroll(function() {
+  //   if ($(window).scrollTop() > 270){
+  //     $('.signature_box').stop().animate({ top: $(window).scrollTop() + top}, 2000);
+  //   }           
+  // });
   
 });
