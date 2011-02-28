cf44b8ce70d86a6769c99c7ccd975f7d8f65251c
Sergio Espeja <sergio.espeja@gmail.com>
2011-02-28 11:02:44 +0100

    Share

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+  def share_texts(proposal, user=nil)
+    @share_title = "Ya he firmado la propuesta #{proposal.name}, ¡únete!"
+    @share_url = proposal_url(proposal)
+    
+    @twitter_text = "Ya he firmado la propuesta #{proposal.name} #{@share_url}"
+    @fb_text = @share_title
+    unless user.nil?
+      @email_subject = "#{@signature.name} ya ha firmado la propuesta de #{proposal.name}"
+      @email_body = "Mifirma.com es una plataforma para desarrollar Iniciativas Legislativas Populares (ILP) a través de firma digital, cumpliendo todos los requisitos especificados en la vigente normativa de aplicación, para poder llevarlas de manera efectiva al Parlamento, y poder crear las leyes que los ciudadanos creemos justas y necesarias.\n#{@signature.name} ha firmado la propuesta de NOMBRE PROPUESTA (enlace), únete con tu firma para crear juntos las leyes que los ciudadanos nos merecemos.\n\nMifirma.com"
+    end    
+  end
+
+
 end
--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,9 +1,12 @@
 class ProposalsController < ApplicationController
   def index
     @proposals = Proposal.all
+    share_texts(@proposals.first)
   end
   
   def show
     @proposal = Proposal.find params[:id]
+    share_texts(@proposal)
   end
+  
 end
