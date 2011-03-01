eb3791d8cf0de3be41ccbbd038cb2d680160ea9b
Sergio Espeja <sergio.espeja@gmail.com>
2011-03-01 21:34:45 +0100

    Fix signature share lightbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,8 @@ class ApplicationController < ActionController::Base
     
     @twitter_text = "Ya he firmado la propuesta #{proposal.name} #{@share_url}"
     @fb_text = @share_title
+    logger.debug "@share_url -> #{@share_url}"
+    logger.debug "@fb_text -> #{@fb_text}"
     unless user.nil?
       @email_subject = "#{@signature.name} ya ha firmado la propuesta de #{proposal.name}"
       @email_body = "Mifirma.com es una plataforma para desarrollar Iniciativas Legislativas Populares (ILP) a través de firma digital, cumpliendo todos los requisitos especificados en la vigente normativa de aplicación, para poder llevarlas de manera efectiva al Parlamento, y poder crear las leyes que los ciudadanos creemos justas y necesarias.\n#{@signature.name} ha firmado la propuesta de NOMBRE PROPUESTA (enlace), únete con tu firma para crear juntos las leyes que los ciudadanos nos merecemos.\n\nMifirma.com"
