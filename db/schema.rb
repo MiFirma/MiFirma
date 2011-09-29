1451844cdbae7477e15d41eead63a1acf5021237
Javier Penya <jpenamar@gmail.com>
2011-09-29 20:41:45 +0200

    staging

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110920151634) do
+ActiveRecord::Schema.define(:version => 20110927201512) do
+
+  create_table "endorsment_proposals", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "endorsment_signatures", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "municipalities", :force => true do |t|
     t.integer  "id_ine"
