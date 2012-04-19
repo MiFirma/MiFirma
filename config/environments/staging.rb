5b985717da5eef2eb9bb8273369f37a28e1e9bcb
Javier Penya <jpenamar@gmail.com>
2012-04-19 18:53:23 +0200

    fixing s3 paperclip

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,6 @@
 PAPERCLIP_CONFIG = {
   :storage => :s3,
   :bucket => 'mifirma-stage',
-	:url => '',
 	:s3_credentials => { :access_key_id     => ENV['S3_KEY'], 
                        :secret_access_key => ENV['S3_SECRET']}	
 }
