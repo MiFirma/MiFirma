1a6ff0f3e97231eef69383919c3790a008114086
Javier Penya <jpenamar@gmail.com>
2012-04-19 18:45:27 +0200

    fixing

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,7 @@
 PAPERCLIP_CONFIG = {
   :storage => :s3,
   :bucket => 'mifirma-stage',
-	:url => :s3_path_url,
+	:url => ':s3_path_url',
 	:s3_credentials => { :access_key_id     => ENV['S3_KEY'], 
                        :secret_access_key => ENV['S3_SECRET']}	
 }
