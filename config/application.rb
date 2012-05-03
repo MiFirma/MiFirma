c64671639e84892e499f9af0b385272c96fcaadb
Javier Penya <jpenamar@gmail.com>
2012-05-03 22:22:46 +0200

    locale for ruby

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module MiFirma2
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-
+		#Adding the below makes it work as expected at heroku
+		config.i18n.locale = :es 
+		
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 		config.action_view.javascript_expansions[:defaults] = %w(jquery)
