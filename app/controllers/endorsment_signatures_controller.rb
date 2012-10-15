60a4c9d899b3c02b4d61c4cb2d7e8aeddb3505ec
Javier Penya <jpenamar@gmail.com>
2012-10-15 20:29:58 +0200

    fixes

--- app/controllers/endorsment_signatures_controller.rb
+++ app/controllers/endorsment_signatures_controller.rb
@@ -4,11 +4,7 @@ class EndorsmentSignaturesController < ApplicationController
   
   def create
     @signature = EndorsmentSignature.new params[:endorsment_signature]
-
-		debugger
-		
 		if @signature.valid?
-			if params[:Certificado] == 'OTROS'
 				tractis_signature_request = ::TractisApi.signature_request_endorsment @signature
 				@signature.update_attribute :tractis_contract_location, tractis_signature_request[:location]
 				if @signature.tractis_contract_location
@@ -19,9 +15,6 @@ class EndorsmentSignaturesController < ApplicationController
 					@signature.destroy
 					redirect_to endorsment_proposal_url(@signature.proposal, :signature => params[:endorsment_signature])
 				end			
-			else
-
-			end
     else
       flash[:error] = @signature.errors.map {|a,m| "#{m.capitalize}"}.uniq.join("<br/>\n")
       redirect_to endorsment_proposal_url(@signature.proposal, :signature => params[:endorsment_signature])
