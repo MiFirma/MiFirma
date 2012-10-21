2523c446e4b5b8df51cad67d6a4fccfcecf7d11c
Javier Penya <jpenamar@gmail.com>
2012-10-21 10:20:31 +0200

    fix endorsment_signature dni validation. Election added

--- app/models/endorsment_signature.rb
+++ app/models/endorsment_signature.rb
@@ -61,8 +61,9 @@ class EndorsmentSignature < Signature
 		end
 	end
 	
+	# Valida que el DNI sea único para este partido en estas elecciones
 	def uniqueness_of_dni
-		if self.signed? and EndorsmentSignature.where("state > 0 and dni = ? and id <> ?",dni,id).count>0
+		if self.signed? and EndorsmentSignature.joins("INNER JOIN proposals ON proposals.id = signatures.proposal_id").where("signatures.state > 0 and signatures.dni = ? and proposals.election_id = ? and signatures.id <> ?",dni,proposal.election_id,id).count>0
 			errors.add :dni, "Sólo puedes firmar un aval una sola vez."
 		end
 	end
