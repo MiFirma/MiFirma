5f7bdf367285e3bd57973d1b67f03acc827fba5c
Javier Penya <jpenamar@gmail.com>
2014-02-16 17:56:15 +0100

    try acumbamail smtp

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,27 @@ require File.expand_path('../application', __FILE__)
 MiFirma2::Application.initialize!
 
 # Tractis Configuration
-TRACTIS_USER = 'tractis'
-TRACTIS_DOMAIN = 'mifirma.com'
-TRACTIS_PASS = ENV['MIFIRMA_PASS']
+#TRACTIS_USER = 'tractis'
+#TRACTIS_DOMAIN = 'mifirma.com'
+#TRACTIS_PASS = ENV['MIFIRMA_PASS']
 
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.acumbamail.com',
+  :port           => '25',
+  :authentication => :plain,
+  :user_name      => ENV['ACUMBAMAIL_USERNAME'],
+  :password       => ENV['ACUMBAMAIL_PASSWORD'],
+}
+
+
+# Sengrid in heroku
+#ActionMailer::Base.smtp_settings = {
+# :address        => 'smtp.sendgrid.net',
+#  :port           => '587',
+#  :authentication => :plain,
+#  :user_name      => ENV['SENDGRID_USERNAME'],
+#  :password       => ENV['SENDGRID_PASSWORD'],
+#  :domain         => 'heroku.com',
+#  :enable_starttls_auto => true
+#}
