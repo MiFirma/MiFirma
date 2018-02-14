35881347336aaa59c2e119191f88b464fa2f767e
Javier <jpenamar@gmail.com>
2018-02-14 20:26:35 +0100

    sendgrid smtp service activated

--- config/environment.rb
+++ config/environment.rb
@@ -10,22 +10,23 @@ MiFirma2::Application.initialize!
 #TRACTIS_PASS = ENV['MIFIRMA_PASS']
 
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.acumbamail.com',
-  :port           => '25',
-  :authentication => :plain,
-  :user_name      => ENV['ACUMBAMAIL_USERNAME'],
-  :password       => ENV['ACUMBAMAIL_PASSWORD'],
-}
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.acumbamail.com',
+#   :port           => '25',
+#   :authentication => :plain,
+#   :user_name      => ENV['ACUMBAMAIL_USERNAME'],
+#   :password       => ENV['ACUMBAMAIL_PASSWORD'],
+# }
 
 
 # Sengrid in heroku
-#ActionMailer::Base.smtp_settings = {
-# :address        => 'smtp.sendgrid.net',
-#  :port           => '587',
-#  :authentication => :plain,
-#  :user_name      => ENV['SENDGRID_USERNAME'],
-#  :password       => ENV['SENDGRID_PASSWORD'],
-#  :domain         => 'heroku.com',
-#  :enable_starttls_auto => true
-#}
+ActionMailer::Base.smtp_settings = {
+ :address        => 'smtp.sendgrid.net',
+ :port           => '587',
+ :domain   		 => 'mifirma.com'
+ :authentication => :plain,
+ :user_name      => ENV['SENDGRID_USERNAME'],
+ :password       => ENV['SENDGRID_PASSWORD'],
+ :domain         => 'heroku.com',
+ :enable_starttls_auto => true
+}
